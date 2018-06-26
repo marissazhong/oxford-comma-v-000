3f86f707edd5254771ff8524bcb2ca1a493ba769
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 02:53:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  result = array[0]
+  if array.length == 2
+    result = "#{array[0]} and #{array[1]}"
+  else
+    array.each_with_index { |word, index|
+      if index != 0
+        index != array.length-1 ? result << ", #{word}" : result << ", and #{word}"
+      end
+    }
+  end
+  return result
 end
+
+puts(oxford_comma(["fiddleheads","okra","kohlrabi"]))
