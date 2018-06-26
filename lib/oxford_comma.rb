55cbae13454f66d26fcc309fca5225c0e22c41ed
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 02:48:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,5 @@ def oxford_comma(array)
   result = ""
   array.each_with_index {|word, index| index != array.length-1 ? result << ", #{word}" : result << ", and #{word}"}
 end
+
+oxford_comma(["fiddleheads","okra","kohlrabi"])
